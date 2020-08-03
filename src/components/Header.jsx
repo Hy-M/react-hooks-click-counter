54706bf3cd793b4455cb27b8044ee9324c58e7b2
Hy-M <humayraa.m@outlook.com>
2020-08-03 16:38:01 +0100

    create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<header>
+			<h1>Click counter</h1>
+			<p>
+				A very simple app that counts how many times a user clicks a button. It
+				is really that simple.
+			</p>
+		</header>
+	);
+};
+
+export default Header;
